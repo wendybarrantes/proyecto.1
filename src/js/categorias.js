1b621e3445decab2ebb7822da03f367329894b03
wendybarrantes <wenbarrantes06@gmail.com>
2024-07-19 00:07:31 -0600

    pagina nueva y redireccion js

---
+++ src/js/categorias.js
@@ -0,0 +1,12 @@
+ 
+ 
+ 
+ function cambiarpagina(){
+    let refereciaSelect = document.getElementById("selec-categoria")
+    let paginaEnlace = refereciaSelect.value
+    if (paginaEnlace) {
+        window.location.href=paginaEnlace
+    }
+    
+ }
+
